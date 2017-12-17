acd4b486f3b6f400073c7471974bd9aa1da4672e
stephencarter.io <stephen@stephencarter.io>
2017-12-17 16:09:10 +1100

    Solution up to Lecture 81

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+	static MusicPlayer instance = null;
+
+	void Awake () {
+		if (instance != null) {
+			Destroy (gameObject);
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad (gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Music Player Start " + GetInstanceID ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
