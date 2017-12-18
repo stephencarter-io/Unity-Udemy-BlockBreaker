1e6f3185290adb64cf11362da627de29e904aec3
stephencarter.io <stephen@stephencarter.io>
2017-12-18 22:26:19 +1100

    Commit at beginning of lecture 87

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Brick : MonoBehaviour {
 
 	public int maxHits;
+	public Sprite[] hitSprites;
 	private int timesHit;
 	private LevelManager levelManager;
 
@@ -26,7 +27,14 @@ public class Brick : MonoBehaviour {
 		timesHit++;
 		if (timesHit >= maxHits) {
 			Destroy(gameObject);
+		} else {
+			LoadSprites();
 		}
+	}
+
+	void LoadSprites() {
+		int spriteIndex = timesHit - 1;
+		this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
 	}
 
 	// TODO Remove this method once we can actually win
